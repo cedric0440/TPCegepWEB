74a675881065b874f8a95a12d62d4208160cc879
Cedric-Tresor <Cedric-Tresor@DESKTOP-8D9PCLN>
2025-02-03 14:37:15 -0500

    teesst

---
+++ TPCegepWEB/Controllers/DepartementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPCegepWEB.Controllers
+{
+    public class DepartementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
