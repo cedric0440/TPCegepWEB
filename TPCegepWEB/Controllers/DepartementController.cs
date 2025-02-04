5b1b5fed3b966acadb02e22442fb6a8120f509f5
Cedric-Tresor <Cedric-Tresor@DESKTOP-8D9PCLN>
2025-02-03 22:34:26 -0500

    etape1

--- TPCegepWEB/Controllers/DepartementController.cs
+++ TPCegepWEB/Controllers/DepartementController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestionCegepWeb.Logics.Controleurs;
+using GestionCegepWeb.Logics.DAOs;
+using GestionCegepWeb.Logics.Modeles;
+using GestionCegepWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TPCegepWEB.Controllers
 {
     public class DepartementController : Controller
     {
-        public IActionResult Index()
+      
+        public IActionResult Index([FromQuery] string nomCegep)
         {
+            // Récupération de la liste des cégeps pour affichage
+            ViewBag.ListeCegeps = CegepControleur.Instance.ObtenirListeCegep();
+
+            if (!string.IsNullOrEmpty(nomCegep))
+            {
+                // Récupération des départements du cégep sélectionné
+                ViewBag.Departements = DepartementRepository.Instance.ObtenirListeDepartement(nomCegep);
+
+                // Récupération du cégep sélectionné
+                var cegepDTO = CegepRepository.Instance.ObtenirCegep(nomCegep);
+                ViewBag.CegepSelectionne = cegepDTO != null ? cegepDTO.Nom : "Inconnu";
+            }
+            else
+            {
+                // Affichage de tous les départements si aucun cégep n'est sélectionné
+                ViewBag.Departements = DepartementRepository.Instance.ObtenirListeDepartement(null);
+                ViewBag.CegepSelectionne = "Tous les cégeps";
+            }
+
             return View();
         }
     }
