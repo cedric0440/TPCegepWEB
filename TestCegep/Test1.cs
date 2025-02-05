8b483e1cf1e5b04d70d0e85372a6ee76929047c0
Cedric-Tresor <Cedric-Tresor@DESKTOP-8D9PCLN>
2025-02-05 10:04:16 -0500

    ETAPE2

---
+++ TestCegep/Test1.cs
@@ -0,0 +1,54 @@
+﻿using GestionCegepWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using TPCegepWEB.Controllers;
+
+namespace TPCegepWEB
+{
+
+[TestClass]
+public sealed class Test1
+{
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        CegepController controleur = new CegepController();
+        ViewResult viewResult = (ViewResult)controleur.Index();
+
+        List<CegepDTO> list = (List<CegepDTO>)viewResult.ViewData["ListeCegeps"];
+        Assert.IsNotNull(list);
+        Assert.AreEqual(3, list.Count);
+        Assert.AreEqual("Cegep Exemple 1", list[0].Nom);
+    }
+        [TestMethod]
+        public void Test_Cegep_Null()
+        {
+            // Arrange
+            DepartementController controleur = new DepartementController();
+
+            // Act
+            ViewResult viewResult = (ViewResult)controleur.Index(null);
+
+            // Assert
+            List<CegepDTO> list = (List<CegepDTO>)viewResult.ViewData["ListeCegeps"];
+            Assert.IsNotNull(list);
+            Assert.IsTrue(list.Count > 0); // Il doit y avoir au moins un cégep disponible.
+        }
+
+        [TestMethod]
+        public void Test_Cegep_Inexistant()
+        {
+            // Arrange
+            DepartementController controleur = new DepartementController();
+
+            // Act
+            ViewResult viewResult = (ViewResult)controleur.Index("Cégep Inconnu");
+
+            // Assert
+            List<CegepDTO> list = (List<CegepDTO>)viewResult.ViewData["ListeCegeps"];
+            Assert.IsNotNull(list);
+            Assert.AreEqual(3, list.Count); // Aucun cégep ne devrait être trouvé.
+        }
+    }
+
+}
