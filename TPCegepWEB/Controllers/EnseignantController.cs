e66a535698c6b6256f7716d91c1c53c7475ddb19
Cedric-Tresor <Cedric-Tresor@DESKTOP-8D9PCLN>
2025-02-06 13:13:33 -0500

    test 1a

---
+++ TPCegepWEB/Controllers/EnseignantController.cs
@@ -0,0 +1,18 @@
+﻿using GestionCegepWeb.Logics.Controleurs;
+using GestionCegepWeb.Logics.DAOs;
+using GestionCegepWeb.Logics.Modeles;
+using GestionCegepWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace TPCegepWEB.Controllers
+{
+    public class EnseignantController : Controller
+    {
+        [Route("Enseignants")]
+        [Route("Enseignants/Index")]
+        [HttpGet]
+        public IActionResult Index(string? nomCegep, string ?nomDepartement)
+        {
+            return View();
+        }
+    }
+}
